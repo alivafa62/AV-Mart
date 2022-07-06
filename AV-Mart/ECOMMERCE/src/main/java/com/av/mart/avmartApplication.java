ee71b1a1037541c51a82cf8c837a7ab3828e2ddb
Ali Vafa <alivafa_62@yahoo.com>
2022-07-06 10:46:34 -0400

    fixed
    some functionality

---
+++ AV-Mart/ECOMMERCE/src/main/java/com/av/mart/avmartApplication.java
@@ -0,0 +1,13 @@
+package com.av.mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class avmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(avmartApplication.class, args);
+	}
+
+}
